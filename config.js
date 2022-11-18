21710be6ddd79312ccf1d3727ded6c210f1b787a
luccanafu <101225093+luccanafu@users.noreply.github.com>
2022-11-17 21:06:59 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  //Cole sua Configuração do Firebase aqui
+    apiKey: "AIzaSyCgTeICmxDgr-Vt6McyKmw8CZtD9UwadNI",
+    authDomain: "pro-71-ca07e.firebaseapp.com",
+    projectId: "pro-71-ca07e",
+    storageBucket: "pro-71-ca07e.appspot.com",
+    messagingSenderId: "858009265476",
+    appId: "1:858009265476:web:4c66179a42ef33213f6311"
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
